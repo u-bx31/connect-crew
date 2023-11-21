ba371a1346c072bdeb2dbd85dbc935a6327fdc71
LabchariOussama <oussama.bx31@gmail.com>
2023-11-21 14:58:25 +0100

    wip

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <main className="container flex min-h-screen flex-col items-center p-24">
       <div className="flex flex-row justify-between w-full shadow-sm p-3 ">
         <h1>connectCrew</h1>
+        <UserButton afterSignOutUrl="/sign-in" />
       </div>
     </main>
   );
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,22 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
+
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // // redirect them to organization selection page
+    // if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/org-selection"){
+    //   const orgSelection = new URL('/org-selection', req.url)
+    //   return NextResponse.redirect(orgSelection)
+    // }
+  },
 
   publicRoutes: ["/", "/api/webhook/clerk"],
   ignoredRoutes: ["/api/webhook/clerk"],
