c7a37985f18768570404d382c1fd80ca06bfcecb
LabchariOussama <oussama.bx31@gmail.com>
2023-11-24 18:53:55 +0100

    wip

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import * as z from "zod";
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
+import { useUploadThing } from "@/lib/uploadthing";
+import { isBase64Image } from "@/lib/utils";
 
 interface props {
 	user: {
@@ -33,6 +35,7 @@ interface props {
 
 const AccountProfile = ({ user, btnTitle }: props) => {
 	const [files, setFiles] = useState<File[]>([]);
+	const {startUpload} = useUploadThing('media');
 	const form = useForm({
 		resolver: zodResolver(userValidation),
 		defaultValues: {
@@ -42,11 +45,21 @@ const AccountProfile = ({ user, btnTitle }: props) => {
 			bio: user?.bio || "",
 		},
 	});
-	const onSubmit = (values: z.infer<typeof userValidation>) => {
-		// Do something with the form values.
-		// ✅ This will be type-safe and validated.
-		console.log(values);
+	const onSubmit = async (values: z.infer<typeof userValidation>) => {
+		const blob = values.profile_photo
+
+		const hasImageChanged = isBase64Image(blob);
+
+		if(hasImageChanged){
+			const imgRes = await startUpload(files)
+			if(imgRes && imgRes[0].url){
+				values.profile_photo = imgRes[0].url
+			}
+		}
+		//update user profile
 	};
+
+	
 	const handleImage = (
 		e: ChangeEvent<HTMLInputElement>,
 		fieldChange: (value: string) => void
