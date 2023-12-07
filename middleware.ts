a6f86e0fd49fe6248230053216ceafbf0e40827c
LabchariOussama <oussama.bx31@gmail.com>
2023-12-07 18:43:21 +0100

    wip

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
 	publicRoutes: ["/api/webhook/clerk"],
-	ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/webhook/clerk"],
+	ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 // Stop Middleware running on static files
 export const config = {
-	matcher: ["/((?!.+.[w]+$|_next).)", "/", "/(api|trpc)(.)"],
+	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
