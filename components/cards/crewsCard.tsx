a692d80b2832d70253ad7b1fd9dea53d27e7755f
LabchariOussama <oussama.bx31@gmail.com>
2023-12-12 16:58:59 +0100

    wip:fix crewCard

--- components/cards/crewsCard.tsx
+++ components/cards/crewsCard.tsx
@@ -18,17 +18,17 @@ function CommunityCard({ id, name, username, imgUrl, bio, members }: Props) {
   return (
     <article className='community-card'>
       <div className='flex flex-wrap items-center gap-3'>
-        <Link href={`/communities/${id}`} className='relative h-12 w-12'>
+        <Link href={`/crews/${id}`} className='relative h-12 w-12'>
           <Image
             src={imgUrl}
-            alt='community_logo'
+            alt='card_logo'
             fill
             className='rounded-full object-cover'
           />
         </Link>
 
         <div>
-          <Link href={`/communities/${id}`}>
+          <Link href={`/crews/${id}`}>
             <h4 className='text-base-semibold text-light-1'>{name}</h4>
           </Link>
           <p className='text-small-medium text-gray-1'>@{username}</p>
@@ -38,7 +38,7 @@ function CommunityCard({ id, name, username, imgUrl, bio, members }: Props) {
       <p className='mt-4 text-subtle-medium text-gray-1'>{bio}</p>
 
       <div className='mt-5 flex flex-wrap items-center justify-between gap-3'>
-        <Link href={`/communities/${id}`}>
+        <Link href={`/crews/${id}`}>
           <Button size='sm' className='community-card_btn'>
             View
           </Button>
